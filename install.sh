22372825d7d467abb2f9a75d297c8bdc607c3a66
Tiffany Lin <tifflin@uber.com>
2021-03-03 19:51:41 +0000

    Update install script to account for devpod

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 ln -sf $HOME/.tmux/tmux.conf $HOME/.tmux.conf
 ln -sf $HOME/.tmux/tmux.conf.local $HOME/.tmux.conf.local
+
+if [ $(whoami) = "user" ]; then
+  ln -sf $HOME/.tmux/tmux.conf.devpod $HOME/.tmux.conf.local
+fi
+
 echo "Installed tmux dotfiles"
