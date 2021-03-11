119ac716b409d6619e20fdd5c48b4e7063d75f72
Tiffany Lin <tifflin@uber.com>
2021-03-11 16:55:27 +0000

    Update intall.sh to account for devpod tmux v3.x being very slow with monorepo

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 ln -sf $HOME/.tmux/tmux.conf $HOME/.tmux.conf
 ln -sf $HOME/.tmux/tmux.conf.local $HOME/.tmux.conf.local
 
-# if [ $(whoami) = "user" ]; then
-#   ln -sf $HOME/.tmux/tmux.conf.devpod $HOME/.tmux.conf.local
-# fi
+if [ $(whoami) = "user" ]; then
+  ln -sf $HOME/.tmux/tmux.conf.devpod $HOME/.tmux.conf.local
+fi
 
 echo "Installed tmux dotfiles"
